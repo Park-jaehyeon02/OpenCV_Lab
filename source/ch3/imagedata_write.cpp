ee0b2a6a2ee9bb63297043984eb451aaf1bfccfe
Park-jaehyeon02 <1234qkr@gmail.com>
2022-05-10 14:41:28 +0900

    add

---
+++ source/ch3/imagedata_write.cpp
@@ -0,0 +1,11 @@
+#include <opencv2/opencv.hpp>
+using namespace std;
+using namespace cv;
+int main()
+{
+	FileStorage fs_r("test.xml", FileStorage::READ);
+	CV_Assert(fs_r.isOpened());
+
+	FileNode node_pic = fs_r["picture"];
+	vector
+}
